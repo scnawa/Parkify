5e5fb00b50592ee44856142b906f6c32b27558b7
SarveshSW <sarvesh.wanzare@icloud.com>
2024-03-06 10:12:45 +1100

    Initial server setup done, other developers can pull and work from their branches.

--- server/app.py
+++ server/app.py
@@ -1,1 +1,38 @@
 #first commit 
+import sys
+import json
+from json import dumps
+from flask import Flask, request, send_from_directory, session, redirect
+from functools import wraps
+from flask_cors import CORS
+#from error import InputError
+import config
+from pymongo import MongoClient
+import certifi
+import helper
+
+
+def defaultHandler(err):
+    response = err.get_response()
+    print('response', err, err.get_response())
+    response.data = dumps({
+        "code": err.code,
+        "name": "System Error",
+        "message": err.get_description(),
+    })
+    response.content_type = 'application/json'
+    return response
+
+APP = Flask(__name__, static_folder = '../static')
+CORS(APP)
+
+APP.config['TRAP_HTTP_EXCEPTIONS'] = True
+APP.register_error_handler(Exception, defaultHandler)
+
+
+@APP.route('/hello')
+def homePage(): 
+    return "Hello"
+
+if __name__ == "__main__": 
+    APP.run(port = config.port)
---
+++ server/config.py
@@ -0,0 +1,4 @@
+from app import APP
+
+port = 8080
+url = f"http://localhost:{port}/"
