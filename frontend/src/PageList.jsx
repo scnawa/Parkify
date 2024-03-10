7d5992627d61ca69f4e09eecdc6465d406bd6ebe
scnawa <51161576+scnawa@users.noreply.github.com>
2024-03-10 14:42:44 +1100

    adjusted imports

--- frontend/src/PageList.jsx
+++ frontend/src/PageList.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
-<<<<<<< HEAD
 import CreateListings from './components/CreateListings';
 import NavBar from './components/NavBar';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import AdminDashboard from './components/AdminDashboard';
-=======
-
-
-import Login from './Components/Login';
-import Signup from './Components/Signup';
-import AdminDashboard from './Components/AdminDashboard';
-import ProfilePage from './Components/profilePage';
->>>>>>> backend-frontend
-
+import ProfilePage from './components/ProfilePage';
 
 const PageList = () => {
   const navigate = useNavigate();
